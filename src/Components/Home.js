544db7927a3b265d257db239e6b99e1cdafca540
Markeya Mckoy-Carree <mcmcarree@gmail.com>
2023-03-14 11:16:17 -0400

    New merge

---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import ErrorMessage from './ErrorMessage';
+
+function Home() {
+    return (
+        <div>
+        //<div className='homepage'>
+         {/* <Searchbar />
+         <section className="videos"></section>
+         <p className='prompt'>No search Result Yet! Please submit a search Above!</p> */}
+        </div>
+    );
+}
+export default Home;
